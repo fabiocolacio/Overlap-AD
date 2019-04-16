bcde8396283ae6f02e5604bd4c219f97371ede05
PizzaPatz <patpongtana@gmail.com>
2019-04-15 19:00:44 -0700

    Added true positive/negative and false positive/negative outputs

--- dynamic_run.py
+++ dynamic_run.py
@@ -44,14 +44,15 @@ if __name__ == "__main__":
 	matplot_color = ['b','g','r','c','m','y','k','w']
 	b1 = 0
 	m = 2
-	i = 1
-	N = 0
-	A = 0
-
+	i = 5
 	benchmarks = 15
+
 	result_list = []
 	result_list.append(-100)
-	
+	result_list.append(-100)
+	result_list.append(-100)
+	result_list.append(-100)
+
 	result_timestamp = []
 
 
@@ -72,40 +73,36 @@ if __name__ == "__main__":
 	dims.append(value)
 
 	cap_data = len(datastamp)
+
 	init_cap = b2 - 2
-
-
 	# Initialize the range of interval to observe data.
 	#	For example, if b2 = 100, we start observe b2+1 because
 	#	b2 has a stable data we trust for cluster. cap_data
 	#	represents the cap of data we want to examine.
 	# total_timestamp = list(range(b2+1, cap_data+1))
 	mid_graph = max(dims[2][b2+1:cap_data+2]) / 2
-
-	
-
-	while(i < cap_data - 2):
+	while(i < cap_data + 1): # Last 2 data will not be classified
 		print('\n\n\n============= Timestamp Number: ', str(i), ' =============')
 		
 		if(i < b2+1):
 			# Definted window's sizes
-			t0_wz = [0, i ]
-			t1_wz = [0, i +1 ]
-			t2_wz = [0, i +2 ]
+			t0_wz = [0, i - 2]
+			t1_wz = [0, i - 1]
+			t2_wz = [0, i]
 
-			t0_size = [0, i ]
-			t1_size = [0, i + 1 ]
-			t2_size = [0, i + 2 ]
+			t0_size = [0, i - 2]
+			t1_size = [0, i - 1]
+			t2_size = [0, i]
 
 
 		else:
-			t0_wz = [b1+i - b2, i]
-			t1_wz = [b1+i - b2, i +1]
-			t2_wz = [b1+i - b2, i +2]
+			t0_wz = [i - b2, i - 2]
+			t1_wz = [i - b2, i - 1]
+			t2_wz = [i - b2, i]
 
-			t0_size = [0, b2]
-			t1_size = [0, b2 + 1]
-			t2_size = [0, b2 + 2]
+			t0_size = [0, b2 - 2]
+			t1_size = [0, b2 - 1]
+			t2_size = [0, b2]
 
 
 		# Get each window into 3 instances
@@ -113,7 +110,6 @@ if __name__ == "__main__":
 		t1_sub_dim = mi.get_sub_dims(dims, t1_wz)
 		t2_sub_dim = mi.get_sub_dims(dims, t2_wz)
 
-		# print(t0_sub_dim)
 
 		# Pre-define the scale for this current iteration 
 		# We take max and min from all 3 windows at this iteration because
@@ -122,7 +118,7 @@ if __name__ == "__main__":
 		#	thus, NO data should be rescaled or move when comparison between
 		#	windows happen.
 		all_min = min(min(t0_sub_dim[2]), min(t1_sub_dim[2]), min(t2_sub_dim[2]))
-		all_max = max(max(t0_sub_dim[2]), max(t1_sub_dim[2]), max(t2_sub_dim[2]))
+		all_max = max(max(t0_sub_dim[2]), max(t1_sub_dim[2]), min(t2_sub_dim[2]))
 
 
 		# # Scale 3 windows based on min and max of their 3 windows.
@@ -130,30 +126,12 @@ if __name__ == "__main__":
 		scaled_t1 = scaler.scale_data(t1_sub_dim[2], all_min, all_max)
 		scaled_t2 = scaler.scale_data(t2_sub_dim[2], all_min, all_max)
 
-		# print('Max observe data : ', observe_2_dims)
-		# print('Scaled t0      : ', scaled_t0)
-		# print('Scaled t1      : ', scaled_t1)
-		# print('Scaled t2      : ', scaled_t2)
-
-		# print('All min: ', all_min)
-		# print('All max: ', all_max)
-
-		# print()
-
-		# # Extra, no need to worry about this for now
-		# MI_main = mi.MINDID_Multi_Dims([scaled_main], m, 15, main_bucket_size)
-		# MI_obs_1 = mi.MINDID_Multi_Dims([scaled_obs_1], m, 15, observe_1_bucket_size)
-		# MI_obs_2 = mi.MINDID_Multi_Dims([scaled_obs_2], m, 15, observe_2_bucket_size)
-
 		# # Removing duplicates so that each precise number is unique and does not
 		# #	cause infinite LCE value
 		scaled_set_t0 = [list(dict.fromkeys(scaled_t0))]
 		scaled_set_t1 = [list(dict.fromkeys(scaled_t1))]
 		scaled_set_t2 = [list(dict.fromkeys(scaled_t2))]
 
-		# print('Non dupe Main      : ', scaled_set_t0)
-		# print('Non dupe Observe 1 : ', scaled_set_t1)
-		# print('Non dupe Observe 2 : ', scaled_set_t2)
 
 		# # Define bucket sizes after duplicates have been removed
 		t0_set_wz = [0, len(scaled_set_t0[0])]
@@ -172,24 +150,19 @@ if __name__ == "__main__":
 		t2_LCE = hp.compute_LCE_index(CS_t2_LCE)
 
 
-		# print('#######################\n')
+		# # Start codition
+		# if( (len(scaled_set_t0[0]) == len(scaled_set_t1[0]) or len(scaled_set_t1[0]) == len(scaled_set_t2[0])) or
+		# 	(CS_t0_LCE[0][t0_LCE] < CS_t1_LCE[0][t1_LCE] or CS_t1_LCE[0][t1_LCE] < CS_t2_LCE[0][t2_LCE])):
+		# 	result_list.append(-100)
 
+		# else:
+		# 	result_list.append(mid_graph)
+		# 	result_timestamp.append(dims[1][i])
 
-		# print('Length t0: ', len(scaled_set_t0[0]))
-		# print('Length t1: ', len(scaled_set_t1[0]))
-		# print('Length t2: ', len(scaled_set_t2[0]))
-
-		# print('t0 LCE: ',CS_main_LCE[0])
-		# print('t1 LCE: ',CS_obs_1_LCE[0])
-		# print('t2 LCE: ',CS_obs_2_LCE[0])
-
-		# print()
-		# print('#######################\n')
-
-
+		# print("t0 window: ", t0_wz)
 		# Start codition
-		if( (len(scaled_set_t0[0]) == len(scaled_set_t1[0]) or len(scaled_set_t1[0]) == len(scaled_set_t2[0])) or
-			(CS_t0_LCE[0][t0_LCE] < CS_t1_LCE[0][t1_LCE] or CS_t1_LCE[0][t1_LCE] < CS_t2_LCE[0][t2_LCE])):
+		if( (len(scaled_set_t1[0]) == len(scaled_set_t2[0]) or len(scaled_set_t1[0]) == len(scaled_set_t2[0])) or
+			(CS_t0_LCE[0][t0_LCE] < CS_t1_LCE[0][t1_LCE]) or (CS_t1_LCE[0][t1_LCE] < CS_t2_LCE[0][t2_LCE])):
 			result_list.append(-100)
 
 		else:
@@ -197,27 +170,12 @@ if __name__ == "__main__":
 			result_timestamp.append(dims[1][i])
 
 		print("t0 window: ", t0_wz)
-		# print('Result', result_list)
 		i += 1
 
-	for d in result_list:
-		if(d == -1):
-			N += 1
-		else:
-			A += 1
 
-	print()
-	print("=== Total classification ===")
-	print('Normal: ', N)
-	print('Anomaly: ', A)
-	print()
+	# result_list.append(-100)
 
-	result_list.append(-100)
-	result_list.append(-100)
-
-
-
-	f = open("./transcript_"+str(args.dataset)+"_"+str(b2)+"_stables_"+str(cap_data)+".txt", "w")
+	f = open("./results/transcript_"+str(args.dataset)+"_"+str(b2)+"_stables_"+str(cap_data)+".txt", "w")
 
 	ground_truth_timestamps = []
 
@@ -230,18 +188,24 @@ if __name__ == "__main__":
  			ground_truth_timestamps.append(windows)
 	print()
 
+
 	ground_truth_datastamp_list = [-100] * len(datastamp)
+	ground_truth_window_list = [-100] * len(datastamp)
 
 	for i in range(1, len(ground_truth_datastamp_list)):
 		if(dims[1][i] in ground_truth_timestamps):
 			ground_truth_datastamp_list[i] = max(dims[2][b2+1:cap_data+2]) / 2
+
 
 	f.write(hp.output('=== Ground Truth Anomaly Windows ==='))
 	with open('./labels/combined_windows.json') as json_file:
 		data = json.load(json_file)
 		for windows in data['realTweets/Twitter_volume_'+str(args.dataset)+'.csv']:
 			f.write(hp.output(str(windows)))
-	print()
+
+	
+
+
 
 	f.write(hp.output('=== Our Anomaly Timestamps ==='))
 	for ts in result_timestamp:
@@ -255,6 +219,8 @@ if __name__ == "__main__":
 
 	origin, = plt.plot(datastamp[0:cap_data], dims[2][0:cap_data], 'o', markersize=np.sqrt(10.), c='b')
 
+
+
 	plt.title('Anomaly Detection on NAB on '+ str(args.dataset))
 	plt.ylabel('Tweet Numbers')
 	plt.xlabel('Timestamp')
@@ -263,4 +229,36 @@ if __name__ == "__main__":
 	dm.mkdir('results')
 	fig.savefig('./results/'+str(args.dataset)+"_"+str(b2)+"_stables_"+str(cap_data), dpi=300)
 	plt.clf()
+
+
+	tp, tn, fp, fn, ud = 0, 0, 0, 0, 0
+
+	for i in range(0,len(datastamp)):
+
+		result, ground_truth = result_list[i], ground_truth_datastamp_list[i]
+
+		if(result == ground_truth): # Positive
+			if(result < 0 and ground_truth < 0): # Both normal
+				tn += 1
+			elif(result > 0 and ground_truth > 0): # Both anomaly
+				tp += 1
+			else:
+				ud += 1
+		else: # Negative
+			if(result > ground_truth): # Our result flagged anomaly on a normal data
+				fp += 1
+			elif(result < ground_truth): # Our result did not flag anomaly on an anomaly data
+				fn += 1
+			else:
+				ud += 1
+
+
+	print()
+	f.write(hp.output('=== Summary ==='))
+	f.write(hp.output('True Negative  :  '+ str(tn)))
+	f.write(hp.output('True Positive  :  '+ str(tp)))
+	f.write(hp.output('False Negative :  '+ str(fn)))
+	f.write(hp.output('False Positive :  '+ str(fp)))
+	f.write(hp.output('Total Data: '+ str(len(result_list))))
+	print()
 	f.close()
