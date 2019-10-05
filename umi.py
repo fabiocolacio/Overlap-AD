8314e8f1da959083be9358b8594215ba27ba8317
Fábio Colacio <fabio.colacio.dev@gmail.com>
2019-10-04 22:33:16 -0700

    Cleaned up umi code and consolidated tests

--- umi.py
+++ umi.py
@@ -93,7 +93,7 @@ def lce(data, min_cluster=2, num_benchmarks=15):
 
     return lce
 
-def classify(datapoint, trusted_data, last_classification, threshold=0):
+def classify(datapoint, trusted_data, last_classification, threshold):
     """Classifies data as NORMAL, ANOMALY, or PENDING.
 
     Args:
@@ -119,7 +119,7 @@ def classify(datapoint, trusted_data, last_classification, threshold=0):
     # Normalize the data to the range [0.0, 1.0]
     for feature in range(feature_count):
         for i in range(len(all_data)):
-            if all_data[i][feature] == feature_mins[feature] and feature_mins[feature] == feature_maxs[feature]:
+            if all_data[i][feature] == feature_mins[feature] == feature_maxs[feature]:
                 all_data[i][feature] = 0.0
             else:
                 all_data[i][feature] = (all_data[i][feature] - feature_mins[feature]) / (feature_maxs[feature] - feature_mins[feature])
@@ -184,3 +184,124 @@ def classify(datapoint, trusted_data, last_classification, threshold=0):
 
     return revision, classification
 
+class classifier:
+    """classifier is a lazy iterator that calls classify repeatedly. It is sugar that can be used as an alternative to using classify() in a loop.
+
+    Args:
+        data: The data to test/classify. This can be a sequence such as a list or numpy array,
+        or it can be a lazy iterator that returns the next datapoint in a streaming context.
+        trusted_data: A deque containing the trusted_data for the system.
+        of list or array, but will internally copied into a deque, as required by classify().
+        threshold: The euclidean distance threshold. See classify() for details.
+    Returns:
+        An iterator that returns a tuple containing (rev, fresh) where rev is the revised classification
+        for the last point, and fresh is the prediction for the new point.
+    """
+
+    def __init__(self, data, trusted_data, threshold):
+        self.data = data
+        self.trusted_data = trusted_data
+        self.threshold = threshold
+        self.data_iter = iter(data)
+        self.last = NORMAL
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        datapoint = np.array((next(self.data_iter),))
+        rev, fresh = classify(datapoint, self.trusted_data, self.last, self.threshold)
+        self.last = fresh
+        return (rev, fresh)
+
+def test(data, labels, window_size, threshold):
+    """test tests this algorithm against a dataset
+
+    Args:
+        data: A list containing the data to test.
+        labels: A list containing labels for data (must be formatted as ANOMALY or NORMAL as defined in this module.
+        window_size: The size of the trusted window to use.
+        threshold: The size of the euclidean distance threshold to use.
+    """
+    data_iter = iter(data)
+
+    fp, fn = 0, 0
+
+    # Extract trusted data from data.
+    # Skips data with anomalous label.
+    trusted_data = deque(maxlen=window_size)
+    i, taken_data, skipped_anomalies = 0, 0, 0
+    for value in data_iter:
+        i = taken_data + skipped_anomalies
+        if taken_data >= window_size:
+            break
+        if labels[i] == ANOMALY:
+            skipped_anomalies += 1
+        else:
+            trusted_data.append((value,))
+            taken_data += 1
+
+    for r, _ in classifier(data_iter, trusted_data, threshold):
+        if r == ANOMALY != labels[i - 1]:
+            fp += 1
+        elif r == NORMAL != labels[i - 1]:
+            fn += 1
+        i += 1
+
+    return (fp, fn, skipped_anomalies)
+
+if __name__ == "__main__":
+    import os
+    import argparse
+    import json
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-if', '--infile', dest='infile', type=str, help='Path to the dataset to test')
+    parser.add_argument('-t', '--trusted-size', dest='trusted_size', type=int, help='The size of the subset of trusted data')
+    parser.add_argument('-s', '--threshold', dest='threshold', type=float, help='The euclidean distance threshold')
+    parser.add_argument('-v', '--verbose', dest='verbose', action='store_true', help='Show detailed output while test is running')
+    parser.add_argument('-c', '--csv', dest='csv', action='store_true', help='Format the program output into csv fomat: (filename,trusted size,threshold,false positives, false negatives,discarded anomalies')
+    parser.add_argument('--twitter', dest='twitter_set',type=str, help='The dataset to use if using twitter (eg. AAPL, GOOG, etc)')
+    parser.add_argument('--yahoo', dest='yahoo', action='store_true', help='Set this flag to parse the csv as a yahoo A1Benchmark')
+    parser.add_argument('--SWaT', dest='SWaT', action='store_true', help='Set this flag to parse the csv as the SWaT data set')
+    parser.set_defaults(twitter_set=False)
+    parser.set_defaults(yahoo=False)
+    parser.set_defaults(SWaT=False)
+    parser.set_defaults(verbose=False)
+    parser.set_defaults(csv=False)
+
+    args = parser.parse_args()
+    infile = args.infile
+    trusted_size = args.trusted_size
+    threshold = args.threshold
+    verbose = args.verbose
+    csv = args.csv
+
+    data, labels = None, None
+    if args.twitter_set:
+        DATA_PATH = './nab/realTweets/realTweets/Twitter_volume_%s.csv'
+        data = np.loadtxt(open(DATA_PATH % (args.twitter_set), 'rb'), delimiter=",", skiprows=1, usecols=1, dtype=int)
+        with open("labels/combined_labels.json") as fh:
+            timestamps = np.loadtxt(open(DATA_PATH % (args.twitter_set), 'rb'), delimiter=",", skiprows=1, usecols=0, dtype=str)
+            labels_raw = fh.read()
+            anomalies = json.loads(labels_raw)["realTweets/Twitter_volume_%s.csv" % (args.twitter_set)]
+            labels = list(map(lambda x: ANOMALY if x in anomalies else NORMAL, timestamps))
+    elif args.SWaT:
+        pass
+    elif args.yahoo:
+        data = np.loadtxt(infile, delimiter=',', skiprows=1, usecols=1, dtype=float)
+        labels = list(map(lambda x: NORMAL if x == 0 else ANOMALY,
+                          np.loadtxt(infile, delimiter=',', skiprows=1, usecols=2, dtype=int)))
+    else:
+        print("No format specified! Run with --help for more info.")
+        os.exit(1)
+
+    false_pos, false_neg, discarded_anomalies = test(data, labels, trusted_size, threshold)
+
+    if csv:
+        print("%10s %10d %10f %10s %10s %10s" % (os.path.basename(infile), trusted_size, threshold, false_pos, false_neg, discarded_anomalies))
+    else:
+        print("Discarded Anomalies:", discarded_anomalies)
+        print("False Positives:", false_pos)
+        print("False Negatives:", false_neg)
+        
